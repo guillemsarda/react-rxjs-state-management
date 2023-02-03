cac90b044b48453ef169cc3b7c2652e3defc27ec
Gui <gsardaparreu@gmail.com>
2023-02-03 12:57:48 +0100

    feat: add typescript

--- src/store-creator.ts
+++ src/store-creator.ts
@@ -1,8 +1,9 @@
 // Source https://isamatov.com/react-rxjs-shared-state/
 import { useEffect, useState } from 'react';
 import { BehaviorSubject } from 'rxjs';
+import { State, Store } from '../lib';
 
-function createStore(states = []) {
+function createStore(states: State<any>[] = []) {
   const subjectsArray = Array(states.length).fill(null);
 
   return function useStore() {
@@ -33,22 +34,26 @@ function createStore(states = []) {
       };
     }, []);
 
+    const storeStatesAcc: Store['storeStates'] = {};
     const storeStates = statesArray.reduce((acc, state) => {
       acc[state.name] = state.reactState[0];
       return acc;
-    }, {});
+    }, storeStatesAcc);
 
+    const methodsAcc: Store['methods'] = {};
     const methods = states.reduce((acc, st, i) => {
-      function setter(fn) {
-        return function (payload) {
+      function setter(fn: State<typeof st.defaultState>['setter']) {
+        return function (payload: any) {
           const nextValue = fn(storeStates[st.name], payload);
           subjectsArray[i].next(nextValue);
         };
       }
-      const capsFirstName = `${st.name.charAt(0).toUpperCase()}${st.name.slice(1)}`;
+      const capsFirstName = `${st.name.charAt(0).toUpperCase()}${st.name.slice(
+        1
+      )}`;
       acc[`set${capsFirstName}`] = setter(st.setter);
       return acc;
-    }, {});
+    }, methodsAcc);
 
     return { storeStates, methods };
   };
